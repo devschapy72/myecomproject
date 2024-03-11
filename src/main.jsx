4d31a96b8ff8ae474bbc716985a570f50db28afe
devschapy72 <devschapy72@gmail.com>
2024-03-11 13:13:32 +0600

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ProductsProvider } from './context/ProductContext.jsx'
-import { FilterProvider } from './context/FilterContext.jsx'
-import { CartProvider } from './context/CartContext.jsx'
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ProductsProvider } from "./context/ProductContext.jsx";
+import { FilterProvider } from "./context/FilterContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-  domain={import.meta.env.VITE_AUTH_DOMAIN}
-  clientId={import.meta.env.VITE_CLIENT_ID}
-  authorizationParams={{
-    redirect_uri: window.location.origin
-  }}
+    // domain={import.meta.env.VITE_AUTH_DOMAIN}
+    // clientId={import.meta.env.VITE_CLIENT_ID}
+    domain="dev-asuie1k5njhnzq4a.us.auth0.com"
+    clientId="aXMmM17bvpXT2XLLLDNOsJZdCiHvsNW4"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
   >
-
-  <ProductsProvider>
-   <FilterProvider>
-   <CartProvider>
-   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-   </CartProvider>
-   </FilterProvider>
-  </ProductsProvider>
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
   </Auth0Provider>
-)
+);
